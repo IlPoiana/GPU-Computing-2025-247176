144a16c9dcf8b9ccae626fd79419655e143c0494
Poiana <emanuele.poiana@\unitn.it>
2025-04-14 11:53:26 +0200

    (spmv): fixed csr random generation and matrix print

--- spmv/csr_rnd.c
+++ spmv/csr_rnd.c
@@ -26,52 +26,52 @@ struct int_matrix gen_rnd_CSR(int x, int y, int p, int binary){
 
     for(int i = 0; i < max_v;i++){
         if(i%y == 0 && i!=0){
-            printf("change row, row count: %d - count: %d\n", row_count, count);
+            printf("change row, row count: %d - count: %d - i: %d\n", row_count, count, i);
             row[row_count] = count;
+            printf("row[%d] = %d\n --- \n", row_count, count);
             ++row_count;
         }
-
-         //generate a value case
+        
         v_buff = rand()%p;
         if(v_buff == 0){
-            printf("gen value, row_count: %d - count: %d - i: %d\n", row_count, count, i);
             if(binary == 1){
                 val[count] = 1;
             }
             else{
                 val[count] = (rand() % binary) + 1; 
             }
-            
-            row[row_count] = count + 1; 
-            
-            col[count] = abs(y - i) % y;
-            count = count + 1;
-            printf("gen value, row_count: %d, count: %d\n", row_count, count);
+
+            if(i >= y)
+                col[count] = abs((y - i) % y);
+            else
+                col[count] = i;
+
+            ++count;
+            row[row_count] = count;
+            printf("gen value: i: %d - row[%d] = %d - col[%d] = %d\n", i,row_count, count,count - 1,col[count - 1]);
         }
+        
+        if(row[row_count] < row[row_count - 1]){
+            row[row_count] = row[row_count - 1];
+        }
+
     }
     int * fval = (int*)malloc(sizeof(int) * count);
     int * frow = (int*)malloc(sizeof(int) * (row_count + 1));
     int * fcol = (int*)malloc(sizeof(int) * count);
-    printf("row_count: %d\n", row_count);
-    for( int i = 0; i < count | i <= x; i++){
-        
-        if(x > count){
-            if(i > 0 && row[i] < row[i - 1]){
-                frow[i] = frow[i - 1];
-            }
-            else{
-                frow[i] = row[i];    
-            }
-        }
-        else {
-            if(i <= x)
-                frow[i] = row[i];
-            fval[i] = val[i];
-            fcol[i] = col[i];
-        }
+    
+    for(int i = 0; i< count; i++){
+        fval[i] = val[i];
+        fcol[i] = col[i];
     }
 
+    for(int i = 0; i<= row_count; i++){
+        frow[i] = row[i];
+    }
+ 
+
     struct int_matrix mtx = {x,y,count,frow,fcol,fval};
+
     return mtx;
 }
 
@@ -104,9 +104,12 @@ int main(int argc, char *args[]){
    
 
     struct int_matrix mtx = gen_rnd_CSR(row_n, col_n,p,binary);
-    PRINT_INT_MTX(mtx,CSR);
-    PRINT_RESULT_ARRAY(mtx.row,"x", mtx.x + 1);
     
+    PRINT_INT_MTX(mtx,CSR);
+    
+    PRINT_RESULT_ARRAY(mtx.row,"row", mtx.x + 1);
+    PRINT_RESULT_ARRAY(mtx.col,"col", mtx.n);
+
     printf("Running sparse matrix multiplication between a 1 vector and a integer value matrix\n");
     
     if (mtx.x == 0 && mtx.y == 0){
